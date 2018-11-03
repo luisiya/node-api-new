c34afe19dac96b1ea31426dd96a4fb7e54654d67
Kravchenko Luidmila <Luisiya@gmail.com>
2018-11-03 13:09:57 +0200

    Corrections2

--- app.js
+++ app.js
@@ -49,6 +49,11 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('User logout. For start using task-board you need to authorize with google')
